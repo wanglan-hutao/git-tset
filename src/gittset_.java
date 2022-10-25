9e01396abe46820d72b4f5988407f6e8db4a934b
wanglan <wanglan010606@gmail.com>
2022-10-25 17:37:16 +0800

    hot-fix-2 commit

--- src/gittset_.java
+++ src/gittset_.java
@@ -7,7 +7,7 @@ public class gittset_ {
     public static void main(String[] args) {
         System.out.println("git-tset");
         System.out.println("git-tset2");
-        System.out.println("git-tset3");
+        System.out.println("git-tset123123");
         System.out.println("git-tset33444444444");
         System.out.println("git-tset44444444444");
     }
