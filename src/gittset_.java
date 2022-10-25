318118d4529a5d9821077abd06d2e7abc735b4fd
王澜 <101966050+wanglan-hutao@users.noreply.github.com>
2022-10-25 18:05:17 +0800

    Update gittset_.java

--- src/gittset_.java
+++ src/gittset_.java
@@ -6,14 +6,6 @@ public class gittset_ {
 
     public static void main(String[] args) {
         System.out.println("git-tset");
-        System.out.println("git-push");
-        System.out.println("git-push");
-        System.out.println("git-push3");
-        System.out.println("git-push4");
-        System.out.println("git-tset2");
-        System.out.println("git-tset12121212");
-        System.out.println("git-tset123123");
-        System.out.println("git-tset33444444444");
-        System.out.println("git-tset44444444444");
+   
     }
 }
